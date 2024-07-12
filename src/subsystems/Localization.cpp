4f86096a33c56ae70a2ef2eda77a5bc5ef864045
PaulBailey-1 <paul.ryan.bailey@gmail.com>
2024-07-12 17:27:42 -0500

    Fixed nan bug in odometry

--- src/subsystems/Localization.cpp
+++ src/subsystems/Localization.cpp
@@ -6,12 +6,6 @@ Localization::Localization() {
 
 void Localization::Update(double dt, DriveBaseFeedback driveVels) {
 
-    double lSlipCoeff = abs(driveVels.lf - driveVels.lb) / std::max(driveVels.lb, driveVels.lf);
-    double rSlipCoeff = abs(driveVels.rf - driveVels.rb) / std::max(driveVels.rb, driveVels.rf);
-
-    _slipCoefficient = lSlipCoeff * (lSlipCoeff / (lSlipCoeff + rSlipCoeff)) +
-                             rSlipCoeff * (rSlipCoeff / (lSlipCoeff + rSlipCoeff));
-
     double vl = (driveVels.lf + driveVels.lb) / 2;
     double vr = (driveVels.rf + driveVels.rb) / 2;
 
@@ -20,9 +14,8 @@ void Localization::Update(double dt, DriveBaseFeedback driveVels) {
 
     Eigen::Vector2d vel(v * cos(_pose.heading), v * sin(_pose.heading));
 
-    double notReallyStdDev = Utils::Clamp(0.5 - _slipCoefficient, 0.0, 0.5);
-    _pose.pos += vel * dt * notReallyStdDev;
-    _pose.heading += omega * dt * notReallyStdDev;
+    _pose.pos += vel * dt;
+    _pose.heading += omega * dt;
 }
 
 void Localization::ReportState(std::string prefix) {
