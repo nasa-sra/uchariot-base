57834c4edaa2bffdd400d2940b3ccae56a9ff954
Paul Bailey <paul.ryan.bailey@gmail.com>
2024-07-25 01:15:30 -0500

    Cleaning up main

--- bob.sh
+++
@@ -1,8 +0,0 @@
-modprobe can
-modprobe can_raw
-modprobe mttcan
-
-sudo ip link set can0 type can bitrate 500000
-sudo ifconfig can0 up
-
-cansend 00000001#000186A0
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 scp ./uChariotBase uchariot@10.93.24.5:~/uchariot-base/build
-# scp -r ../paths/ uchariot@10.93.24.9:~/uchariot-base/build/paths
-# scp -r ./ uchariot@10.93.24.4:~/uchariot-base
+# scp -r ../paths/ uchariot@10.93.24.5:~/uchariot-base/build/paths
